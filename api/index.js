67cd6bbb646b6f261cf33796429c13b55e28ef40
Come Back <ceo@comebackagency.com>
2025-03-18 01:10:53 +0200

    update vercel config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,24 @@
 const app = require("../app")
 
-module.exports = app
+const {connectDb} = require("../orm");
+const lightspeedCron = require("../utils/lightspeed-cron");
+
+async function assertDatabaseConnectionOk() {
+    console.log(`Checking database connection...`);
+    try {
+        await connectDb();
+        console.log('Database connection OK!');
+    } catch (error) {
+        console.log('Unable to connect to the database:');
+        process.exit(1);
+    }
+}
+
+async function init() {
+    await assertDatabaseConnectionOk();
+    await lightspeedCron.synchronizeProducts();
+}
+
+init();
+
+module.exports = app;
