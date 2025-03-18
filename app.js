43a13e2b082926cf20be5d476cba2a7bc39552be
Come Back <ceo@comebackagency.com>
2025-03-18 13:37:27 +0200

    update vercel config

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.get('/', (req, res) => res.json({ status: "ok - homePage" }));
 //         });
 // });
 
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
